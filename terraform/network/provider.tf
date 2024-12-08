f6855c430eef07150a9410240185039dd6b16d1f
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2024-12-08 16:32:41 -0300

    Update provider.tf

--- terraform/network/provider.tf
+++ terraform/network/provider.tf
@@ -25,6 +25,9 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = 
-  secret_key = 
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
+
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
