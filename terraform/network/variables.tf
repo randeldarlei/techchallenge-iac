055e7a394a16554ac6f3c29f6da818880c8609b2
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2024-12-09 19:29:34 -0300

    Update variables.tf

--- terraform/network/variables.tf
+++ terraform/network/variables.tf
@@ -1,9 +1,1 @@
-variable "AWS_ACCESS_KEY_ID" {
-  description = "AWS Access Key ID"
-  type        = string
-}
 
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "AWS Secret Access Key"
-  type        = string
-}
