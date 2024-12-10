0fd53a5ac091684e66452381cebf3f1d4c5b4318
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2024-12-09 21:03:21 -0300

    Update provider.tf

--- terraform/network/provider.tf
+++ terraform/network/provider.tf
@@ -17,5 +17,13 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
+}
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {
+  default = "us-east-1"
 }
