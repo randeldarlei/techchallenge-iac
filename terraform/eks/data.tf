542e017df71976c64246c118432faf89d0a4b6be
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2024-12-11 20:25:14 -0300

    Update data.tf

--- terraform/eks/data.tf
+++ terraform/eks/data.tf
@@ -10,6 +10,10 @@ data "terraform_remote_state" "network" {
   }
 }
 
+output "vpc_id" {
+  value       = data.terraform_remote_state.network.outputs.vpc_id
+}
+
 output "private_subnet_ids" {
   value       = data.terraform_remote_state.network.outputs.private_subnet_ids
 }
