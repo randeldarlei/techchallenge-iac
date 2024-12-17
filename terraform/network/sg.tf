135cc108e2a4cdcc7e6dad4dadd918a3751677d2
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2024-12-17 19:34:53 -0300

    Update sg.tf

--- terraform/network/sg.tf
+++ terraform/network/sg.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
-  description = "Allow TLS and HTTP/HTTPS traffic for EKS and outbound traffic"
+  description = "Allow TLS, PostgreSQL traffic for EKS, and outbound traffic"
   vpc_id      = aws_vpc.cluster_vpc.id
 
   tags = {
@@ -8,49 +8,47 @@ resource "aws_security_group" "allow_tls" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "10.0.0.0/16"
+# Permite conexões ao RDS a partir da VPC do EKS (10.0.0.0/16)
+resource "aws_security_group_rule" "allow_postgres_from_eks" {
+  type              = "ingress"
   from_port         = 5432
-  ip_protocol       = "tcp"
   to_port           = 5432
+  protocol          = "tcp"
+  cidr_blocks       = ["10.0.0.0/16"] # VPC CIDR Block do EKS
+  security_group_id = aws_security_group.allow_tls.id
 }
 
+# Permite tráfego HTTP
 resource "aws_security_group_rule" "allow_http" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.allow_tls.id
-}
-
-resource "aws_security_group_rule" "allow_https" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.allow_tls.id
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_outbound_to_internet" {
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = "0.0.0.0/0"
+  type              = "ingress"
   from_port         = 80
-  to_port           = 443
-  ip_protocol       = "tcp"
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.allow_tls.id
 }
 
-resource "aws_security_group_rule" "allow_egress_internet" {
-  security_group_id = aws_security_group.allow_tls.id
-  type              = "egress"
+# Permite tráfego HTTPS
+resource "aws_security_group_rule" "allow_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.allow_tls.id
+}
+
+# Permite tráfego de saída para a internet
+resource "aws_security_group_rule" "allow_outbound" {
+  type              = "egress"
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.allow_tls.id
 }
 
+# Permite comunicação interna entre recursos na VPC do EKS
 resource "aws_security_group_rule" "allow_internal_communication" {
   type              = "ingress"
   from_port         = 0
