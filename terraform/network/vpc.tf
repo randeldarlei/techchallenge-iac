ea895c04718afe1b4b16bc17e569d4dcd1c48f2c
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2024-12-18 17:06:09 -0300

    Update vpc.tf

--- terraform/network/vpc.tf
+++ terraform/network/vpc.tf
@@ -1,9 +1,13 @@
 resource "aws_vpc" "cluster_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
   tags = {
     Name = "Cluster VPC"
   }
 }
+
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.cluster_vpc.id
