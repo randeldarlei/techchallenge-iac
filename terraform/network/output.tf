8edebb88c7d45019baf0b69fccdadc46a528dc54
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2024-12-18 16:22:18 -0300

    Update output.tf

--- terraform/network/output.tf
+++ terraform/network/output.tf
@@ -12,23 +12,9 @@ output "public_subnet_ids" {
   ]
 }
 
-output "private_subnet_ids" {
-  description = "IDs das subnets privadas"
-  value       = [
-    aws_subnet.private_cluster_subnet_1.id,
-    aws_subnet.private_cluster_subnet_2.id,
-    aws_subnet.private_cluster_subnet_3.id
-  ]
-}
-
 output "public_route_table_id" {
   description = "ID da tabela de rotas pública"
   value       = aws_route_table.public_cluster_route_table.id
-}
-
-output "private_route_table_id" {
-  description = "ID da tabela de rotas privada"
-  value       = aws_route_table.private_cluster_route_table.id
 }
 
 output "aws_security_group_id" {
