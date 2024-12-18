496418100eb8d258c2e7d658d32a5df7c669517b
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2024-12-18 16:15:34 -0300

    Update sg.tf

--- terraform/network/sg.tf
+++ terraform/network/sg.tf
@@ -8,13 +8,13 @@ resource "aws_security_group" "allow_tls" {
   }
 }
 
-# Permite conexões ao RDS a partir da VPC do EKS (10.0.0.0/16)
-resource "aws_security_group_rule" "allow_postgres_from_eks" {
+# Permite conexões ao RDS a partir da Internet
+resource "aws_security_group_rule" "allow_postgres_from_internet" {
   type              = "ingress"
   from_port         = 5432
   to_port           = 5432
   protocol          = "tcp"
-  cidr_blocks       = ["10.0.0.0/16"] # VPC CIDR Block do EKS
+  cidr_blocks       = ["<SEU-IP-PÚBLICO>/32"]
   security_group_id = aws_security_group.allow_tls.id
 }
 
@@ -38,40 +38,12 @@ resource "aws_security_group_rule" "allow_https" {
   security_group_id = aws_security_group.allow_tls.id
 }
 
-# Permite tráfego de saída para a internet
-resource "aws_security_group_rule" "allow_outbound" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.allow_tls.id
-}
-
-# Permite comunicação interna entre recursos na VPC do EKS
-resource "aws_security_group_rule" "allow_internal_communication" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 65535
-  protocol          = "tcp"
-  cidr_blocks       = ["10.0.0.0/16"]
-  security_group_id = aws_security_group.allow_tls.id
-}
-
+# Permite tráfego para o backend (porta 8080)
 resource "aws_security_group_rule" "allow_http_8080" {
-  type              = "ingress" # Entrada no backend
+  type              = "ingress"
   from_port         = 8080
   to_port           = 8080
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"] # Permitir de qualquer IP
-  security_group_id = aws_security_group.allow_tls.id
-}
-
-resource "aws_security_group_rule" "allow_outbound_8080" {
-  type              = "egress" # Saída do backend
-  from_port         = 8080
-  to_port           = 8080
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"] # Permitir saída para qualquer IP
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.allow_tls.id
 }
