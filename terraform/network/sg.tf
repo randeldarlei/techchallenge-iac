4a2cf66aaac0d31d052978080cae97ae80640a8a
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2024-12-18 16:21:25 -0300

    Update sg.tf

--- terraform/network/sg.tf
+++ terraform/network/sg.tf
@@ -8,42 +8,62 @@ resource "aws_security_group" "allow_tls" {
   }
 }
 
-# Permite conexões ao RDS a partir da Internet
+# Permite conexões ao RDS a partir de qualquer IP (subnets públicas)
 resource "aws_security_group_rule" "allow_postgres_from_internet" {
   type              = "ingress"
   from_port         = 5432
   to_port           = 5432
   protocol          = "tcp"
-  cidr_blocks       = ["<SEU-IP-PÚBLICO>/32"]
+  cidr_blocks       = ["0.0.0.0/0"] # Permite acesso público ao RDS
   security_group_id = aws_security_group.allow_tls.id
 }
 
-# Permite tráfego HTTP
+# Permite tráfego HTTP ao backend
 resource "aws_security_group_rule" "allow_http" {
   type              = "ingress"
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"] # Permite acesso público HTTP
   security_group_id = aws_security_group.allow_tls.id
 }
 
-# Permite tráfego HTTPS
+# Permite tráfego HTTPS ao backend
 resource "aws_security_group_rule" "allow_https" {
   type              = "ingress"
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"] # Permite acesso público HTTPS
   security_group_id = aws_security_group.allow_tls.id
 }
 
-# Permite tráfego para o backend (porta 8080)
+# Permite tráfego de saída para a internet
+resource "aws_security_group_rule" "allow_outbound" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"] # Permite saída irrestrita
+  security_group_id = aws_security_group.allow_tls.id
+}
+
+# Permite tráfego interno entre recursos na VPC (EKS e RDS)
+resource "aws_security_group_rule" "allow_internal_communication" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = ["10.0.0.0/16"] # Tráfego interno da VPC
+  security_group_id = aws_security_group.allow_tls.id
+}
+
+# Permite tráfego HTTP ao backend na porta 8080
 resource "aws_security_group_rule" "allow_http_8080" {
   type              = "ingress"
   from_port         = 8080
   to_port           = 8080
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = ["0.0.0.0/0"] # Permite acesso público na porta 8080
   security_group_id = aws_security_group.allow_tls.id
 }
